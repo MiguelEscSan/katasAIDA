00ed41eaa726eaec1d75430aeed71abade95aa7b
Miguel Escobedo Santana (AIDA) <mescobedo@gda.dis>
2025-02-06 11:49:35 +0000

    added test for checking when 1 is given

--- FizzBuzz/FizzBuzzShould.cs
+++ FizzBuzz/FizzBuzzShould.cs
@@ -5,9 +5,19 @@ namespace FizzBuzz
     public class FizzBuzzShould
     {
       
+      [Test]
+      public void GiveAnEmptyStringWhenOneIsProvided(){
+        var number = 1;
+        var expected = "";
+
+        var result = new FizzBuzzClass().FizzBuzz(number);
+
+        result.ShouldBe(expected);
+
+      }
       
         [Test]
-        public void GivenANumberDivisableByThreeShouldGiveFizz()
+        public void GiveANumberDivisableByThreeShouldGiveFizz()
         {
             var number = 3;
             var expected  = "Fizz";
