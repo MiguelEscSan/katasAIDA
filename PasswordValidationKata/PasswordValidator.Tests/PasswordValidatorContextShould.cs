1cddc4b3209083ced09bb80d3b0aa28a77e556f8
elenamoralesgil <elena.morales104@alu.ulpgc.es>
2025-02-11 10:03:37 +0000

    refactor: added mockPasswordValidator that passes if a test is valid

--- PasswordValidationKata/PasswordValidator.Tests/PasswordValidatorContextShould.cs
+++ PasswordValidationKata/PasswordValidator.Tests/PasswordValidatorContextShould.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata;
 using System.Threading.Tasks;
 using FluentAssertions;
 
@@ -14,22 +15,47 @@ namespace PasswordValidator.Tests
         public void give_false() {
             string password = "";
 
-            var result = new PasswordValidatorContext().ValidatePassword(password);
+            var result = new PasswordValidatorContext(new MockPasswordValidator(false)).ValidatePassword(password);
 
             result.Should().BeFalse();
             
+        }
+
+        [Test]  
+        public void give_true() {
+            string password = "hello";
+
+            var result = new PasswordValidatorContext( new MockPasswordValidator(true)).ValidatePassword(password);
+
+            result.Should().BeTrue();
+        }
+
+        class MockPasswordValidator : IPasswordValidator {
+
+            bool isValid;
+    
+            public MockPasswordValidator(bool isValid) {
+                this.isValid = isValid;
+            }
+            public bool ValidatePassword(string password){
+
+                return this.isValid;
+            }
         }
         
     }
 
     public class PasswordValidatorContext {
 
-        public PasswordValidatorContext() {
+        IPasswordValidator passwordValidator;
+
+        public PasswordValidatorContext( IPasswordValidator passwordValidator) {
+            this.passwordValidator = passwordValidator;
 
         }
 
         public bool ValidatePassword(string password) {
-            return false;
+            return passwordValidator.ValidatePassword("");
         }
     }
 }
