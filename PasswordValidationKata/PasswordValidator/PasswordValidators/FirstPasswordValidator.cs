47044cd1917182a02c02f9fd4921726065e9c730
elenamoralesgil <elena.morales104@alu.ulpgc.es>
2025-02-12 12:10:19 +0000

    refactor: deleted comments

--- PasswordValidationKata/PasswordValidator/PasswordValidators/FirstPasswordValidator.cs
+++ PasswordValidationKata/PasswordValidator/PasswordValidators/FirstPasswordValidator.cs
@@ -5,13 +5,10 @@ namespace PasswordValidator
     public class FirstPasswordValidator : IPasswordValidator{
         public bool ValidatePassword(string password){
             
-
-            // if(password.Length < 8) return false;
             if(new LengthValidationRule().Validate(password) is false) return false;
 
             if(!password.Any(char.IsLower)) return false;
 
-            //if(!password.Any(char.IsUpper)) return false;
             if(new UpperValidationRule().Validate(password) is false) return false;
 
             if(PasswordValidatorHelper.PasswordHasANumericCharacter(password) is false) return false;
