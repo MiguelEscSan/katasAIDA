29999da55ace9c2b351ff721deb438a30f0e204e
Miguel Escobedo Santana (AIDA) <mescobedo@gda.dis>
2025-02-13 08:43:51 +0000

    test: check when password not contains lowercase

--- PasswordValidationKata/PasswordValidator.Tests/PasswordValidatorShould.cs
+++ PasswordValidationKata/PasswordValidator.Tests/PasswordValidatorShould.cs
@@ -4,8 +4,6 @@ namespace PasswordValidator.Tests;
 public class PasswordValidatorShould
 {
 
-
-
         [Test]
         public void not_allowed_when_password_does_not_have_minimum_length(){
             var password = "";
@@ -14,7 +12,6 @@ public class PasswordValidatorShould
             var result = new  PasswordValidatorClass(rules).ValidatePassword(password);
 
             result.ShouldBe(false);
-        
         }
 
         [Test]
@@ -32,6 +29,16 @@ public class PasswordValidatorShould
         public void not_allowed_when_password_does_not_contain_uppercase(){
             var password = "holaestosonmasde22_ocho";
             List<ValidationRule> rules = [ new HasAtLeastminimumCharacters(8), new HasCapitalLetter()];
+
+            var result = new  PasswordValidatorClass(rules).ValidatePassword(password);
+
+            result.ShouldBe(false);
+        }
+
+        [Test]
+        public void not_allowed_when_password_does_not_contain_lowercase(){
+            var password = "HOLAMUNDOTENEMOS22LETRAS_JAAJA";
+            List<ValidationRule> rules = [ new HasAtLeastminimumCharacters(8), new HasCapitalLetter(), new HasLowerCaseCharacter()];
 
             var result = new  PasswordValidatorClass(rules).ValidatePassword(password);
 
