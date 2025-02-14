b337f240e9eb27e28c4e5b3468630aa57c044446
elenamoralesgil <elena.morales104@alu.ulpgc.es>
2025-02-14 10:55:27 +0000

    refactor: created turnDirection function

--- MarsRovers/MarsRoversSimple/MarsRoversSimple.cs
+++ MarsRovers/MarsRoversSimple/MarsRoversSimple.cs
@@ -10,18 +10,19 @@ public class MarsRoversSimple : MarsRovers {
 
     public string Execute(string command) {
         foreach(var letter in command.Split()) {
-        
-            // if(letter == "M" && this.currentPosition.currentOrientation == "N") {
-            //     this.currentPosition.currentCoordinates.y += 1;
-            // }
+    
 
             if(letter == "M"){
                 this.MoveForward();
             }
 
-            if(letter == "L" && this.currentPosition.currentOrientation == "N"){
-                this.currentPosition.currentOrientation = "W";
+            // if(letter == "L" && this.currentPosition.currentOrientation == "N"){
+            //     this.currentPosition.currentOrientation = "W";
 
+            // }
+
+            if(letter == "L"){
+                this.TurnDirection();
             }
         }
         return this.currentPosition.ToString();
@@ -30,6 +31,12 @@ public class MarsRoversSimple : MarsRovers {
     public void MoveForward(){
         if(this.currentPosition.currentOrientation == "N") {
                 this.currentPosition.currentCoordinates.y += 1;
+        }
+    }
+
+    public void TurnDirection(){
+        if(this.currentPosition.currentOrientation == "N"){
+            this.currentPosition.currentOrientation = "W";
         }
     }
 }
