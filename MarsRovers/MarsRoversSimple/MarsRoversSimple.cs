8ffea35d0afee3fcb5cf9963e301ab8795ca9b19
MiguelEscSan <miguelescobedosantana2@gmail.com>
2025-02-17 13:01:39 +0000

    refactor: delete unused function and classes

--- MarsRovers/MarsRoversSimple/MarsRoversSimple.cs
+++ MarsRovers/MarsRoversSimple/MarsRoversSimple.cs
@@ -4,10 +4,6 @@ namespace MarsRoversSimple;
 
 public class MarsRoversSimple : MarsRovers {
 
-    public Position currentPosition {get; set;}
-
-    public GameBoard gameBoard{get; set;}
-
     public MarsRoversState MarsRoversState;
 
     public MarsRoversSimple() {
@@ -21,45 +17,6 @@ public class MarsRoversSimple : MarsRovers {
             this.MarsRoversState = this.MarsRoversState.Execute(command[position].ToString());
         }
         return this.MarsRoversState.ToString();
-    }
-
-    public void MoveForward() {
-
-        switch(this.currentPosition.currentOrientation) {
-         
-            case "N":
-                this.currentPosition.currentCoordinates.y = NextCoordinates( this.currentPosition.currentCoordinates.y +1 , this.gameBoard.height);
-                break;
-            case "S":
-                this.currentPosition.currentCoordinates.y = NextCoordinates( this.currentPosition.currentCoordinates.y -1 , this.gameBoard.height);
-                break;
-            case "E":
-                this.currentPosition.currentCoordinates.x = NextCoordinates( this.currentPosition.currentCoordinates.x +1 , this.gameBoard.width);
-                break;
-            case "W":
-                this.currentPosition.currentCoordinates.x = NextCoordinates( this.currentPosition.currentCoordinates.x -1 , this.gameBoard.width);
-                break;
-            default:
-                return;
-        }
-    }
-
-    public  int NextCoordinates(int value, int max){
-        if (value < 0) return max -1;
-        if (value >= max) return 0;
-        return value;
-    }
-}
-    
-
-public class GameBoard {
-
-    public int height {get;}
-    public int width {get;}
-
-    public GameBoard(int height = 10, int width = 10) {
-        this.height = height;
-        this.width = width;
     }
 
 }
