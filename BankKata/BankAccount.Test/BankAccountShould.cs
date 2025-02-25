e40419fc592da72def30722f46b7ccb72737d536
MiguelEscSan <miguelescobedosantana2@gmail.com>
2025-02-25 12:26:43 +0000

    refactor: now we use string instead another sut at the tests

--- BankKata/BankAccount.Test/BankAccountShould.cs
+++ BankKata/BankAccount.Test/BankAccountShould.cs
@@ -14,7 +14,6 @@ public class BankAccountShould
     [Test]
     public void give_empty_when_there_is_no_money_at_the_account() {
         var NewAccount = new Account();
-        var Expected = new Account();
 
         string ActualOutput = "";
         string ExpectedOutput = "";
@@ -27,45 +26,36 @@ public class BankAccountShould
         }
         using(StringWriter stringWriter = new StringWriter()) {
             Console.SetOut(stringWriter);
-            Expected.printStatement();
-            ExpectedOutput = stringWriter.ToString();
-        } 
-        // string ExpectedOutput = "Date || Amount || Balance\n";
+            System.Console.WriteLine("Date || Amount || Balance");
+            ExpectedOutput = stringWriter.ToString();     
+        }
 
         ActualOutput.ShouldBe(ExpectedOutput);
-
         Console.SetOut(Console.Out);
     }
 
     [Test]
-    public void give_empty_when_depositing_zero(){
+    public void give_50_when_depositing_50(){
         var NewAccount = new Account();
-        var Expected = new Account(){
-            BankStatement = new Stack<Transaction>(new[] { new Transaction(0) })  
-        };
-        NewAccount.deposit(0);
+        NewAccount.deposit(50);
+
         string ActualOutput = "";
         string ExpectedOutput = "";
         using(StringWriter stringWriter = new StringWriter()) {
             Console.SetOut(stringWriter);
             NewAccount.printStatement();
             ActualOutput = stringWriter.ToString();
-
-            
         }
         using(StringWriter stringWriter = new StringWriter()) {
             Console.SetOut(stringWriter);
-            Expected.printStatement();
-            ExpectedOutput = stringWriter.ToString();
-        } 
-        // string ExpectedOutput = "Date || Amount || Balance\n";
+            System.Console.WriteLine("Date || Amount || Balance");
+            System.Console.WriteLine("25/02/2025 || 50 || 50");
+            ExpectedOutput = stringWriter.ToString();     
+        }
 
         ActualOutput.ShouldBe(ExpectedOutput);
 
         Console.SetOut(Console.Out);
     }
-
-
-
 
 }
