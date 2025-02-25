35a3716bf4d6b956df6ba0185263be3ebe40a1d0
elenamoralesgil <elena.morales104@alu.ulpgc.es>
2025-02-25 12:57:03 +0000

    feat: Withdraw works

--- BankKata/BankAccount/Account.cs
+++ BankKata/BankAccount/Account.cs
@@ -1,32 +1,32 @@
 namespace BankAccount;
-
+ 
 public class Account : AccountService {
-
+ 
     public List<Transaction> BankStatement;
-
+ 
     public Account() {
         BankStatement = new List<Transaction>();
     }
-
+ 
     public void deposit(int amount) {
         BankStatement.Add(new Transaction(amount));
     }
-
-
+ 
+ 
     public void withdraw(int amount) {
-
+        BankStatement.Add(new Transaction(-amount));
     }
-
+ 
     public void printStatement() {
         System.Console.WriteLine("Date || Amount || Balance");
-
+ 
         List<int> CurrentBalances = CalculateCurrentBalances();
-        
+ 
         for(int i = BankStatement.Count - 1; i >= 0; i--) {
             System.Console.WriteLine($"{BankStatement[i].ToString()} || {CurrentBalances[i]}");
         }
     }
-
+ 
     private List<int> CalculateCurrentBalances() {
         List<int> Balances = new List<int>();
         int amount = 0;
