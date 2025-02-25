b316e16fcab8fb6e55866d5262b49d02305a0076
MiguelEscSan <miguelescobedosantana2@gmail.com>
2025-02-25 12:34:55 +0000

    feat: calculate currentBalance in a moment

--- BankKata/BankAccount.Test/BankAccountShould.cs
+++ BankKata/BankAccount.Test/BankAccountShould.cs
@@ -58,4 +58,31 @@ public class BankAccountShould
         Console.SetOut(Console.Out);
     }
 
+
+    [Test]
+    public void give_120_when_depositing_50_and_70(){
+        var NewAccount = new Account();
+        NewAccount.deposit(50);
+        NewAccount.deposit(70);
+
+        string ActualOutput = "";
+        string ExpectedOutput = "";
+        using(StringWriter stringWriter = new StringWriter()) {
+            Console.SetOut(stringWriter);
+            NewAccount.printStatement();
+            ActualOutput = stringWriter.ToString();
+        }
+        using(StringWriter stringWriter = new StringWriter()) {
+            Console.SetOut(stringWriter);
+            System.Console.WriteLine("Date || Amount || Balance");
+            System.Console.WriteLine("25/02/2025 || 70 || 120");
+            System.Console.WriteLine("25/02/2025 || 50 || 50");
+            ExpectedOutput = stringWriter.ToString();     
+        }
+
+        ActualOutput.ShouldBe(ExpectedOutput);
+
+        Console.SetOut(Console.Out);
+    }
+
 }
