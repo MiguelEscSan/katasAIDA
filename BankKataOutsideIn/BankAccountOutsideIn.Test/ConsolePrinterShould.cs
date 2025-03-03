9cb0d8bd6e5298f37bac7c7c211d93dc72b7908c
elenamoralesgil <elena.morales104@alu.ulpgc.es>
2025-03-03 11:26:12 +0000

    feat: now ConsolePrinter prints bank statement with transactions

--- BankKataOutsideIn/BankAccountOutsideIn.Test/ConsolePrinterShould.cs
+++ BankKataOutsideIn/BankAccountOutsideIn.Test/ConsolePrinterShould.cs
@@ -22,4 +22,26 @@ public class ConsolePrinterShould {
         ActualOutput.ShouldBe(expecetedOutput);
     }
 
+    [Test]
+
+    public void print_the_bank_statement_of_an_account_with_transactions(){
+        string expecetedOutput = "Date || Amount || Balance\r\n14/01/2012 || -500 || 2500\r\n13/01/2012 || 2000 || 3000\r\n10/01/2012 || 1000 || 1000\r\n";
+
+        List<Transaction> transactionRows = [ 
+            new Transaction(new DateTime(2012, 1,10), 1000),
+            new Transaction(new DateTime(2012, 1,13), 2000),
+            new Transaction(new DateTime(2012, 1,14), -500),
+        ];
+        List<StatementRow> statementRows = [
+            new StatementRow(transactionRows[0], 1000),
+            new StatementRow(transactionRows[1], 3000),
+            new StatementRow(transactionRows[2], 2500),
+        ];
+        printer.Print(statementRows);
+        string ActualOutput = printer.GetPrintedContent();
+
+        ActualOutput.ShouldBe(expecetedOutput);
+    
+    }
+
 }
--- BankKataOutsideIn/BankAccountOutsideIn/ConsolePrinter.cs
+++ BankKataOutsideIn/BankAccountOutsideIn/ConsolePrinter.cs
@@ -7,11 +7,20 @@ public class ConsolePrinter: Printer{
  
     public void Print(List<StatementRow> StatementInformation) {
         output.AppendLine("Date || Amount || Balance");
+        for (int index = StatementInformation.Count - 1; index >= 0; index--) {
+            output.AppendLine($"{FormatTransaction(StatementInformation[index].transaction)} || {StatementInformation[index].CurrentBalance}");
+        }
         System.Console.WriteLine(GetPrintedContent());
     }
  
     public string GetPrintedContent() {
         return output.ToString();
+    }
+     private string FormatTransaction(Transaction transaction) {
+        return $"{FormatDate(transaction.Date)} || {transaction.Amount}";
+    }
+    private string FormatDate(DateTime Date) {
+        return Date.ToString("dd/MM/yyyy");
     }
 }
  
