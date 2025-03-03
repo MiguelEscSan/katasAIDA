fbfc8312e71d6e84fdcd08da5e8bb1e4e7f86b4d
MiguelEscSan <miguelescobedosantana2@gmail.com>
2025-03-03 11:43:20 +0000

    refactor: create GetTransactionsWithBalances

--- BankKataOutsideIn/BankAccountOutsideIn/Account.cs
+++ BankKataOutsideIn/BankAccountOutsideIn/Account.cs
@@ -5,31 +5,32 @@ public class Account : AccountService
     private TransactionRepository transactionRepository;
     private DateProvider dateProvider;
     private Printer printer;
-    public Account(TransactionRepository transactionRepository, DateProvider dateProvider, Printer printer){
+    public Account(TransactionRepository transactionRepository, DateProvider dateProvider, Printer printer) {
         this.transactionRepository = transactionRepository;
         this.dateProvider = dateProvider;
         this.printer = printer;
     }
-    public void deposit(int amount)
-    {
+    public void deposit(int amount) {
         transactionRepository.Save(new Transaction(dateProvider.GetDate(), amount));
     }
 
-    public void printStatement() {
-        var currentBalance = 0;
-        var statementRows = transactionRepository
-            .orderByDateTime()
-            .Select(transaction => {
-                currentBalance += transaction.Amount;
-                var statementRow = new StatementRow(transaction, currentBalance);
-                return statementRow;
-            }).ToList();
-        this.printer.Print(statementRows);
-    }
-
-
-    public void withdraw(int amount)
-    {
+    public void withdraw(int amount) {
         transactionRepository.Save(new Transaction(dateProvider.GetDate(), -amount));
     }
+
+    public void printStatement() {
+        var sortedTransactions = transactionRepository.orderByDateTime();
+        var statementsInfo = GetTransactionsWithBalances(sortedTransactions);
+        this.printer.Print(statementsInfo);
+    }
+
+    private List<StatementRow> GetTransactionsWithBalances( List<Transaction> SortedTransactions) {
+        int currentBalance = 0;     
+        return SortedTransactions
+                .Select(transaction => {  
+                    currentBalance += transaction.Amount;
+                    return new StatementRow(transaction, currentBalance); 
+                }).ToList();       
+    } 
+    
 }
